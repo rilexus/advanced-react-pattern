1e8762014296dd7bb348b54f84c85d73158e770a
Stanislav Panchenko <stanip@hotmail.de>
2019-12-03 22:20:27 +0100

    refactor

--- src/components/props-collection/Input.js
+++ src/components/props-collection/Input.js
@@ -1,9 +1,14 @@
 import React, {useCallback, useMemo, useState} from 'react';
 
 const Input = ({children}) => {
+	
+	
+	
 	const [value , _setValue ] = useState('');
 	const [isDirty , setDirty ] = useState(false);
 	const [touched , setTouched ] = useState(false);
+	
+	
 	
 	const setValue = useCallback((e) => {
 		const {value: _value} = e.target;
@@ -11,20 +16,24 @@ const Input = ({children}) => {
 		if (!isDirty) setDirty(() => true);
 	},[value]);
 	
+	
+	
 	const touch = useCallback(()=> {
 		if (!touched) setTouched(true)
 	},[touched]);
 	
-	const getProps = useCallback(({onChange, ...rest}) => {
-		return {
-			value,
-			onChange: (e) => {
-				onChange && onChange(e);
-				setValue(e)
-			},
-			onClick: touch
-		}
-	},[value]);
+	
+	
+	const getProps = useCallback(({onChange, ...rest}) => ({
+		value,
+		onChange: (e) => {
+			onChange && onChange(e);
+			setValue(e)
+		},
+		onClick: touch
+	}),[value]);
+	
+	
 	
 	return children({isDirty, touched, getProps})
 };
