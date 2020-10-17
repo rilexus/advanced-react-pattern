4a227b32b5fedc702b94f67fbe24308e12d45c4f
Stan <stanip@hotmail.de>
2020-10-17 20:10:04 +0200

    docs(Command): added an usage comment

--- src/hooks/command/index.js
+++ src/hooks/command/index.js
@@ -3,6 +3,31 @@ import { useCallback, useState } from "react";
 const isPromise = (command) => typeof command.then === "function";
 const isThunk = (command) => typeof command === "function";
 
+/*
+* Usage:
+* const simpleAddCommand = createCommand((state, number) => ({
+  ...state,
+  value: state.value + number,
+}));
+
+const addThunk = (num) => {
+  return (execute) => execute(add(num));
+};
+
+const Commando = () => {
+  const [state, execute] = useCommand({ value: 1 });
+  return (
+    <div>
+      <div>{state.value}</div>
+      <div>
+        <button onClick={() => execute(simpleAddCommand(1))}>
+          Add one (1)
+        </button>
+      </div>
+    </div>
+  );
+};
+* */
 const useCommand = (initialState) => {
   const [state, setState] = useState(() => initialState);
 
