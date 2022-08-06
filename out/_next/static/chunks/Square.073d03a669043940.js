7e7729934f22ac3cfc3b34a688d9d0b1da6a0b54
rilexus <stanip@hotmail.de>
2022-08-06 23:35:20 +0200

    Deploy gh-pages

---
+++ out/_next/static/chunks/Square.073d03a669043940.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[145],{6066:function(e,i,o){o.r(i);o(7294);var r=o(5893);i.default=function(e){var i=e.color,o=void 0===i?"blue":i,t=e.width,l=void 0===t?"4rem":t,n=e.height,u=void 0===n?"4rem":n;return(0,r.jsx)("div",{style:{backgroundColor:o,height:u,width:l,display:"inline-block"}})}}}]);
